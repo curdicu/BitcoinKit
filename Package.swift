a1e7875f2630b54bbdf59fd783ba2086f337f981
curdicu <curdicu@gmail.com>
2025-02-28 21:42:22 +0800

    *

--- Package.swift
+++ Package.swift
@@ -14,19 +14,19 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/curdicu/BitcoinCore.git", .branch( "main")),
-        .package(url: "https://github.com/curdicu/Hodler.git", .branch( "main")),
-        .package(url: "https://github.com/curdicu/HdWalletKit.git", .branch( "main")),
-        .package(url: "https://github.com/curdicu/HsToolKit.git", .branch( "main")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/horizontalsystems/Hodler.Swift.git", .upToNextMajor(from: "2.0.1")),
+        .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.1")),
+        .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.5")),
     ],
     targets: [
         .target(
             name: "BitcoinKit",
             dependencies: [
-                .product(name: "BitcoinCore", package: "BitcoinCore"),
-                .product(name: "Hodler", package: "Hodler"),
-                .product(name: "HdWalletKit", package: "HdWalletKit"),
-                .product(name: "HsToolKit", package: "HsToolKit"),
+                .product(name: "BitcoinCore", package: "BitcoinCore.Swift"),
+                .product(name: "Hodler", package: "Hodler.Swift"),
+                .product(name: "HdWalletKit", package: "HdWalletKit.Swift"),
+                .product(name: "HsToolKit", package: "HsToolKit.Swift"),
             ]
         ),
     ]
